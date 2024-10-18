832076e56ebfdc45953634d98cd6448241a77e82
dothanhnhutis <dothanhnhutis@gmail.com>
2024-10-18 11:30:25 +0700

    update...

--- client/app/testfetch/page.tsx
+++ client/app/testfetch/page.tsx
@@ -1,23 +1,32 @@
 "use client";
 import React from "react";
 
-import axios from "axios";
+import axios, { isAxiosError } from "axios";
+import { FetchApiError, http } from "./fetch-api";
 
 const TestFetchPage = () => {
   React.useEffect(() => {
     const startFetch = async () => {
       try {
-        const data = await axios.post<{ message: string }>(
-          "http://localhost:4000/api/v1/auth/signin",
+        const { data: datas } = await http.post<{ message: string }>(
+          "/auth/signin",
           {
             email: "gaconght@gmail.com",
             password: "@Abc1231233",
           }
         );
-        console.log(data);
+        console.log("--------data--------");
       } catch (error) {
-        console.log(error);
+        if (error instanceof FetchApiError) {
+          console.log("--------error--------");
+          console.log(error.toJSON());
+        } else if (isAxiosError(error)) {
+          console.log(error.response?.data);
+        } else {
+          console.log(error);
+        }
       }
+      console.log(new Error("Nhut"));
     };
     startFetch();
   }, []);
